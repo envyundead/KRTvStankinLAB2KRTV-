711911e3383dfcc199066c4320a350cf0ce52087
envyundead <133106249+envyundead@users.noreply.github.com>
2023-06-20 20:34:14 +0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,6 @@
+function equal(){
+let x = document.getElementById("fnum").value;
+let y = document.getElementById("snum").value;
+    let q = (x==y) ? 'equal' : 'not equal';
+ alert(q);
+}
---
+++ script2.js
@@ -0,0 +1,4 @@
+function admin(){
+document.getElementById("admin").innerText= `Admin: ${document.getElementById('name').value}`
+// "Admin: " + document.getElementById("name").value;
+}
